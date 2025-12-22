306d811a12391e2f99e006ae12cc34a1aa52b157
firsty_rahma <nisarhm34@gmail.com>
2025-12-22 14:33:04 +0700

    Fix code to get the header text

--- pages/multiple_windows_page.py
+++ pages/multiple_windows_page.py
@@ -1,31 +1,33 @@
+"""Multiple Windows page object - FIXED"""
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 class MultipleWindowsPage(BasePage):
     # Locators
-    HEADING = (By.TAG_NAME)
+    HEADING = (By.TAG_NAME, "h3")
     CLICK_HERE_LINK = (By.LINK_TEXT, "Click Here")
-
+    
     def get_heading_text(self):
-        """Get page heading text"""
-        return self.get_text(self.HEADING)
+        """Get page heading text - FIXED"""
+        element = self.get_element(self.HEADING)
+        return element.text
     
     def click_here(self):
         """Click the 'Click Here' link to open new window"""
         self.click_element(self.CLICK_HERE_LINK)
-
+    
     def wait_for_new_window(self, original_handles):
         """Wait for new window to open"""
         self.wait.until(lambda d: len(d.window_handles) > len(original_handles))
-
+    
     def switch_to_new_window(self, original_handle):
         """Switch to the newly opened window"""
         for handle in self.driver.window_handles:
             if handle != original_handle:
                 self.driver.switch_to.window(handle)
                 break
-            
+    
     def switch_to_window(self, handle):
         """Switch to specific window handle"""
         self.driver.switch_to.window(handle)
